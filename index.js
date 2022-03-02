5ab6c82e3c684503713394ad2ed042fa43980913
Trey Herrera <therrera152@gmail.com>
2022-03-02 10:19:35 -0600

    commit tree 3ad11df9a043a6ac423e5645dad911dfb1926602

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(array, name) {
+   return array.filter(
+    (match) => match.toUpperCase() === name.toUpperCase()
+   );
+}
+// returns all drivers that match the passed in name
+//uppercase helps sense it is case sensitive
+
+function fuzzyMatch(array, string) {
+    return array.filter (
+    (match) => match.toUpperCase().indexOf(string.toUpperCase()) === 0
+    );
+}
+
+function matchName(array,matchedName){
+    return array.filter((record) =>record.name === matchedName);
+}
